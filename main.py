45794e6f7e641dd6d6b700bd5d2dafde82b93425
Zakaria <zakaria.kreifeur@live.fr>
2024-12-17 10:23:14 +0100

    mise à jour du prompt

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import streamlit as st
 client = OpenAI()
 
 
-value = st.text_input("Prompt")
+value = st.text_input("Prompt....")
+
 if(value):
-    txt = st.header("Waiting for api...")
-    
+    with (st.chat_message("user")):
+        st.write(value)
+
+    with (st.chat_message("assistant")):
+        txt = st.header("Waiting for api...")
+
     completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "user", "content": "write a haiku about ai"},
+        {"role": "user", "content": value},
     ]
 )
     txt.text(completion.choices[0].message.content)
